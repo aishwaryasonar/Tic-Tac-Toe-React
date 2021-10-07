3f602fa9582d001849f3e44897f0c9ecd5188943
aishwaryasonar <aishwaryasonar@outlook.com>
2021-10-07 16:39:39 +0530

    Updates

---
+++ precache-manifest.b95273046db43010698955a2f92b0698.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d340fc08c74247e55147fb2c468a33af",
+    "url": "/Tic-Tac-Toe-React/index.html"
+  },
+  {
+    "revision": "640e0055f2228628d3b3",
+    "url": "/Tic-Tac-Toe-React/static/css/main.a3fe9f27.chunk.css"
+  },
+  {
+    "revision": "d4beeb07b80aeb6b3f4c",
+    "url": "/Tic-Tac-Toe-React/static/js/2.f47b7d6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe-React/static/js/2.f47b7d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "640e0055f2228628d3b3",
+    "url": "/Tic-Tac-Toe-React/static/js/main.55f48589.chunk.js"
+  },
+  {
+    "revision": "0f645f25ca90b5d8bf77",
+    "url": "/Tic-Tac-Toe-React/static/js/runtime-main.e7258889.js"
+  }
+]);
